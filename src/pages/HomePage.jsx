d120cb10695e930a18490da0cb37767f73926516
NexusLegends4 <aaronjamesquijano022104@gmail.com>
2026-04-20 19:59:16 +0800

    images test

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -14,9 +14,9 @@ const HomePage = () => {
     : "/log-in";
 
   const images = [
-    "https://scontent.fmnl9-2.fna.fbcdn.net/v/t39.30808-6/495034226_122127788288749963_6494358927456626875_n.jpg",
-    "https://scontent.fmnl9-1.fna.fbcdn.net/v/t39.30808-6/494369075_122128145408749963_4990497671908646009_n.jpg",
-    "https://scontent.fmnl9-5.fna.fbcdn.net/v/t39.30808-6/494762994_122127788384749963_4263195623448948223_n.jpg",
+    "https://scontent.fmnl25-7.fna.fbcdn.net/v/t39.30808-6/495034226_122127788288749963_6494358927456626875_n.jpg?stp=cp6_dst-jpegr_tt6&_nc_cat=111&ccb=1-7&_nc_sid=7b2446&_nc_eui2=AeHgf-UXE9hvmS_XFx53Fx7XAdK01uX6x0EB0rTW5frHQWW34lM-6uhLeE0V4QcWZgFq9viXtfwtsE_v82sGxF8w&_nc_ohc=8MOfALyUSWkQ7kNvwFrSN-J&_nc_oc=AdrrDAJZniq-4n2o2659DWNZVtGJqO-dpXg7CW_ahb0wm0tg2xiKzS7N_V6rvRjUqz8&_nc_zt=23&se=-1&_nc_ht=scontent.fmnl25-7.fna&_nc_gid=v2ifRWvwjMt93w6GQ5EyXg&_nc_ss=7a3a8&oh=00_Af2OjoD4FrG5VqNGjAUc17SiLHj8HLHzNIUbIevLIr_2TA&oe=69EBDD52",
+    "https://scontent.fmnl25-5.fna.fbcdn.net/v/t39.30808-6/481303337_122113740890749963_5078274125687890241_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=7b2446&_nc_eui2=AeGCRDmPtAf2scB9p3ZLkGuH6iRisONI3WnqJGKw40jdaVqr_odvIx6NI_XMBD50lGZUHJqDDhC9G-25meE3XxmQ&_nc_ohc=NE7MKzoHImgQ7kNvwHqdaoR&_nc_oc=Ado16LEhyIXn6BWjyHCuJkIUE78rhkOVWrBU7jfYiX5MYYX4CHbbldlxS9yMIhOJjOA&_nc_zt=23&_nc_ht=scontent.fmnl25-5.fna&_nc_gid=cdl6Co87kxqKqEPyQcSWLQ&_nc_ss=7a3a8&oh=00_Af1YA2idv2iRICg_J_kKgcnH3f3QyXbzNrAWnon54kS4Iw&oe=69EBE59E",
+    "https://scontent.fmnl25-8.fna.fbcdn.net/v/t39.30808-6/494369075_122128145408749963_4990497671908646009_n.jpg?stp=cp6_dst-jpegr_tt6&_nc_cat=110&ccb=1-7&_nc_sid=7b2446&_nc_eui2=AeE4GS5HLw2x1OxTTMtFZjeu-VCDRKTPXQf5UINEpM9dB4rpld3sHW41x5Fk60G9wJ_UqB8qWvvEQzav1LHqItFT&_nc_ohc=dy1X0zRrwZEQ7kNvwGZ4kuj&_nc_oc=Adomw14PpRSzT43F6sDSNVj6ADB8bc19s2fd5tgY8mxFQInQjC41nEKaCdI4Y3Tzbp4&_nc_zt=23&se=-1&_nc_ht=scontent.fmnl25-8.fna&_nc_gid=s4AvOq-eNHIu8nf3FASWLg&_nc_ss=7a3a8&oh=00_Af1il1xMyQI9MRG7_nxt2-zqxL5EvV06SQRfRL3Q5Jk0Ug&oe=69EBF195",
   ];
 
   return (
